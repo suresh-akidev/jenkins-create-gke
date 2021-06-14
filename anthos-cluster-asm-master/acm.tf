5b2c0390f5a003b3b1277021f765a65600d8a5eb
sr494 <suresh.r@dxc.com>
2021-06-14 11:18:33 +0530

    updated

--- anthos-cluster-asm-master/acm.tf
+++ anthos-cluster-asm-master/acm.tf
@@ -1,7 +1,7 @@
 module "acm" {
 source           = "terraform-google-modules/kubernetes-engine/google//modules/acm"
 
-  project_id       = "	bcm-pcidss-devops-jenkins"
+  project_id       = "bcm-pcidss-devops-jenkins"
   cluster_name     = "gke-anthos-sur"
   location         = module.gke.location
   cluster_endpoint = module.gke.endpoint
--- anthos-cluster-asm-master/hub.tf
+++ anthos-cluster-asm-master/hub.tf
@@ -1,7 +1,7 @@
 module "hub" {
 source           = "terraform-google-modules/kubernetes-engine/google//modules/hub"
 
-  project_id       = "	bcm-pcidss-devops-jenkins"
+  project_id       = "bcm-pcidss-devops-jenkins"
   cluster_name     = "gke-anthos-sur"
   location         = module.gke.location
   cluster_endpoint = module.gke.endpoint
